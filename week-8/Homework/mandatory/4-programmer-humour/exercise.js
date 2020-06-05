51e9625105264b5166a0d468b708e3f509bcb59d
Sabia <sabiamushtaq2000@gmail.com>
2020-06-05 12:22:28 +0100

    ex 4

--- week-8/Homework/mandatory/4-programmer-humour/exercise.js
+++ week-8/Homework/mandatory/4-programmer-humour/exercise.js
@@ -1,7 +1,8 @@
 fetch('https://xkcd.now.sh/?comic=latest').then(response => {
     console.log(response);
     return response.json();
-}) .then(data => {
+}) 
+.then(data => {
     console.log(data);
     let imgSrc = data.img
     let div = document.getElementById("main")
