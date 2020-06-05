70df6381e8bd206aaa7a5db8f40d98372de7b2e4
Sabia <sabiamushtaq2000@gmail.com>
2020-06-05 12:20:37 +0100

    fix ex 4

--- week-8/Homework/mandatory/4-programmer-humour/exercise.js
+++ week-8/Homework/mandatory/4-programmer-humour/exercise.js
@@ -1,11 +1,12 @@
 fetch('https://xkcd.now.sh/?comic=latest').then(response => {
     console.log(response);
     return response.json();
-})
-
-.then(data => {
+}) .then(data => {
     console.log(data);
     let imgSrc = data.img
+    let div = document.getElementById("main")
     let image = document.createElement("img")
+    div.appendChild(image)
+    console.log(imgSrc)
     image.src = imgSrc
 })
